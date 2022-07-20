9a9587f2480695deac0a1d682b5168c852173b39
Jesse Tomchak <jtomchak@gmail.com>
2022-07-20 12:18:05 -0700

    starting form items
    Co-authored-by: Jessica Wilkins  <jdwilkin4@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = React.useState<string[]>([]);
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputRef?.current?.value) {
+      setTodos([...todos, inputRef.current.value]);
+      inputRef.current.value = "";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor=""></label>
+        <input name="todoItem" ref={inputRef} type="text" />
+      </form>
+      <ul>
+        {todos.map((todoItem) => (
+          <li key={todoItem}>{todoItem}</li>
+        ))}
+      </ul>
     </div>
   );
 }
